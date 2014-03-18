75a4bb603200d7cd91b1284cef6219bc65e1edc8
VlikeBanana <AndrolinRoot@googlemail.com>
2014-03-18 14:20:13 -0400

    Revert "Readd AOKPStats"
    Was causing Settings FC
    This reverts commit d58b0a90a625309047cfd0b84d630845fd11c917.
    
    Conflicts:
    	res/drawable-hdpi/ic_aokp_stats_notif.png
    	res/drawable-xhdpi/ic_aokp_stats_notif.png
    	res/values/strings_custom.xml
    
    Change-Id: If36b6f7930a26251bba63e4bf09fe41bebdef6a3
    
    Conflicts:
    	res/values/strings_custom.xml

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1155,5 +1155,4 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
-    public static class AnonymousStatsActivity extends Settings { /* empty */ }
 }
